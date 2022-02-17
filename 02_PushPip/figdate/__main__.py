d8e0a2c60a12e58c84f5170925e37c9d5c8d9a64
Ilya Savitsky <ipsavitsky234@gmail.com>
2022-02-17 10:29:39 +0300

    figdate parameter order fixed

--- 02_PushPip/figdate/__main__.py
+++ 02_PushPip/figdate/__main__.py
@@ -5,7 +5,7 @@ if __name__ == '__main__':
     import locale
     locale.setlocale(locale.LC_TIME, 'ru_RU.UTF-8')
     if len(sys.argv) == 3:
-        print(figdate(sys.argv[2], sys.argv[1]))
+        print(figdate(sys.argv[1], sys.argv[2]))
     elif len(sys.argv) == 2:
         print(figdate(format=sys.argv[1]))
     else:
