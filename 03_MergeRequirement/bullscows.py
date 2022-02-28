1932266486e40147846b90a146a70871397235fc
Ilya Savitsky <ipsavitsky234@gmail.com>
2022-03-01 01:55:40 +0300

    gameplay update

--- 03_MergeRequirement/bullscows.py
+++ 03_MergeRequirement/bullscows.py
@@ -1,8 +1,24 @@
 import textdistance as td
+from random import choice
+
 
 def bullscows(guess: str, secret: str) -> tuple[int, int]:
-    # TODO: sorensen bad????
     hamming_len = td.Hamming().similarity(guess, secret)
     bag_len = td.Bag().similarity(guess, secret)
     return (hamming_len, bag_len)
 
+
+def gameplay(ask: callable, inform: callable, words: list[str]) -> int:
+    secret = choice(words)
+    while (curword := ask('Введите слово: ', words)) != secret:
+        inform('Быки: {}, Коровы: {}', *bullscows(curword, secret))
+    # what to do here?!?!?
+    print(123)
+
+
+def ask(prompt: str, valid: list[str] = None) -> str:
+    pass
+
+
+def inform(format_string: str, bulls: int, cows: int) -> None:
+    pass
